cebdbb8fd3c37b85c7fd691f94bf92266482574a
Dylan Hackworth <dylhack@outlook.com>
2021-01-05 20:51:25 -0600

    Finished Users Table

--- src/Common/Database/tables/Users.cs
+++ src/Common/Database/tables/Users.cs
@@ -6,6 +6,9 @@ namespace Modmail.Database.Tables
 {
   public class Users : Table<ModmailUser>
   {
+    static string COLUMNS => "id";
+    static string INSERTION => "@id";
+    static sbyte OID => 0;
     const string INIT = @"
     CREATE TABLE IF NOT EXISTS modmail.users (
       id BIGINT NOT NULL
@@ -17,6 +20,50 @@ namespace Modmail.Database.Tables
 
     public Users(ref string connStr) : base("Users", connStr)
     {}
+
+    /// <summary>
+    /// <c>Store</c> store a new user
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns>A boolean representing whether or not the user was stored
+    /// successfully</returns>
+    public async Task<bool> Store(ModmailUser user)
+    {
+      NpgsqlConnection connection = new NpgsqlConnection();
+      NpgsqlCommand cmd = new NpgsqlCommand(
+        $"INSERT INTO modmail.users ({COLUMNS}) VALUES ({INSERTION})",
+        connection);
+
+      cmd.Parameters.AddWithValue("id", user.ID);
+
+      return await Execute(cmd);
+    }
+
+    /// <summary>
+    /// <c>Remove</c> remove a user
+    /// </summary>
+    /// <param name="userID"></param>
+    /// <returns>A boolean representing whether or not the user was removed
+    /// </returns>
+    public async Task<bool> Remove(long userID)
+    {
+      NpgsqlConnection connection = new NpgsqlConnection();
+      NpgsqlCommand cmd = new NpgsqlCommand(
+        "DELETE FROM modmail.users WHERE id=@id",
+        connection);
+
+      cmd.Parameters.AddWithValue("id", userID);
+
+      return await Execute(cmd);
+    }
+
+    protected override ModmailUser Read(NpgsqlDataReader reader)
+    {
+      return new ModmailUser
+      {
+        ID = reader.GetInt64(0),
+      };
+    }
 
     protected override async Task Prepare()
     {
